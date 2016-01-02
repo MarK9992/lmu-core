0d81aa5409946b87e46946fdef892bde69486453
Marc Karassev <marc.karassev@yahoo.fr>
2016-01-02 16:10:03 +0100

    Ajout d'une interface pour le controlleur (spécification + possibilité de mocker)

---
+++ lmu-core/src/main/java/org/lucci/lmu/LmuCore.java
@@ -0,0 +1,11 @@
+package org.lucci.lmu;
+
+/**
+ * @author Benjamin Benni, Marc Karassev
+ * 
+ * This interface represents the app's API.
+ */
+public interface LmuCore {
+	
+	void analyzePackage(String packageName);
+}
---
+++ lmu-core/src/main/java/org/lucci/lmu/LmuCoreController.java
@@ -0,0 +1,15 @@
+package org.lucci.lmu;
+
+/**
+ * @author Benjamin Benni, Marc Karassev
+ * 
+ * This class is an implementation of the app's API.
+ */
+public class LmuCoreController implements LmuCore {
+
+	@Override
+	public void analyzePackage(String packageName) {
+		// TODO Auto-generated method stub
+		
+	}
+}
