1ab50ff8b992d4e2a5dbb01e11baaf65f7309462
Marc Karassev <marc.karassev@yahoo.fr>
2016-01-09 19:42:21 +0100

    forcing test to throw an exception if no file produced

--- lmu-core/src/test/java/org/lucci/lmu/ModelExporterTest.java
+++ lmu-core/src/test/java/org/lucci/lmu/ModelExporterTest.java
@@ -9,8 +9,11 @@ import org.lucci.lmu.output.ModelExporter;
 import org.lucci.lmu.output.ModelExporterImpl;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.*;
 
 /**
  * @author Marc Karassev
@@ -22,20 +25,29 @@ public class ModelExporterTest {
     private static IModel sampleOrgModel;
     private static ModelExporter modelExporter;
 
-    // Tests
+    // Set-ups and tear-downs
 
     @BeforeClass
     public static void setUp() throws IOException, ParseError {
         URL url = Thread.currentThread().getContextClassLoader().getResource("sample-org.jar");
         File file = new File(url.getPath());
+        DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(LmuCore.DEFAULT_OUTPUT_PATH));
 
         sampleOrgModel = new JarFileAnalyser().createModel(file);
         modelExporter = new ModelExporterImpl();
+        for (Path filePath: stream) {
+            Files.delete(filePath);
+        }
     }
 
+    // Tests
+
     @Test
-    public void exportToPdfTest() {
+    public void exportToPdfTest() throws FileNotFoundException {
+        FileInputStream fileInputStream;
+
         modelExporter.exportToFile(sampleOrgModel, LmuCore.DEFAULT_OUTPUT_PATH + "/sample-org.pdf");
-        // check by yourself now
+        new FileInputStream(LmuCore.DEFAULT_OUTPUT_PATH + "/sample-org.pdf");
+        // check the file content by yourself now
     }
 }
