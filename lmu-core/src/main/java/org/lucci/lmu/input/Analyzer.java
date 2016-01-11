a43c02b1667f78f16f63de96ed203fe2c1117a49
Marc Karassev <marc.karassev@yahoo.fr>
2016-01-11 21:38:49 +0100

    moving some shared methods to superclass

--- lmu-core/src/main/java/org/lucci/lmu/input/Analyzer.java
+++ lmu-core/src/main/java/org/lucci/lmu/input/Analyzer.java
@@ -1,17 +1,24 @@
 package org.lucci.lmu.input;
 
-import org.lucci.lmu.model.Entities;
-import org.lucci.lmu.model.Entity;
-import org.lucci.lmu.model.IModel;
-import org.lucci.lmu.model.InheritanceRelation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.lucci.lmu.model.*;
+import toools.io.FileUtilities;
 import toools.io.file.RegularFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.*;
 import java.util.*;
 
 /**
  * @author Marc Karassev
  */
 public abstract class Analyzer extends ModelBuilder {
+
+    // Constants
+
+    public static final Logger LOGGER = LogManager.getLogger();
 
     // Attributes
 
@@ -86,5 +93,155 @@ public abstract class Analyzer extends ModelBuilder {
         }
 
         return e;
+    }
+
+    protected void initAttributes(Class<?> clazz, Entity entity, IModel model) {
+        for (Field field : clazz.getDeclaredFields()) {
+            // if the field is not static
+            if ((field.getModifiers() & Modifier.STATIC) == 0) {
+                // System.err.println(clazz.getName() + " " + field.getName());
+                Type fieldType = field.getGenericType();
+
+                if (fieldType instanceof ParameterizedType) {
+                    for (Type parameterType : ((ParameterizedType) fieldType).getActualTypeArguments()) {
+                        if (parameterType instanceof Class<?>) {
+                            Class<?> parameterClass = (Class<?>) parameterType;
+                            Entity parameterEntity = getEntity(model, parameterClass);
+
+                            if (!parameterEntity.isPrimitive()) {
+                                AssociationRelation rel = new AssociationRelation(parameterEntity, entity);
+                                rel.setType(AssociationRelation.TYPE.AGGREGATION);
+                                //
+                                // if
+                                // (!field.getName().equalsIgnoreCase(parameterEntity.getName()
+                                // + 's'))
+                                // {
+                                // rel.setLabel(field.getName());
+                                // }
+                                //
+                                rel.setLabel(field.getName());
+                                rel.setCardinality("0..n");
+                                model.addRelation(rel);
+                            }
+                        }
+                    }
+                } else {
+                    Entity fieldTypeEntity = getEntity(model, field.getType());
+
+                    if (fieldTypeEntity != null) {
+                        if (fieldTypeEntity.isPrimitive()) {
+                            Attribute att = new Attribute();
+                            att.setName(field.getName());
+                            att.setVisibility(getVisibility(field));
+                            att.setType(fieldTypeEntity);
+                            entity.getAttributes().add(att);
+                        } else {
+                            AssociationRelation rel = new AssociationRelation(fieldTypeEntity, entity);
+                            rel.setType(AssociationRelation.TYPE.AGGREGATION);
+
+                            // if (fieldTypeEntity.getName().contains("$"))
+                            // System.out.println("inner class: " +
+                            // fieldTypeEntity.getName());
+
+                            if (fieldTypeEntity.getName().toUpperCase().indexOf(field.getName().toUpperCase()) < 0) {
+                                rel.setLabel(field.getName());
+                            }
+
+                            rel.setCardinality("1");
+                            model.addRelation(rel);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    protected void initOperations(Class<?> clazz, Entity entity, IModel model) {
+        try {
+            for (Method method : clazz.getDeclaredMethods()) {
+                Entity typeEntity = getEntity(model, method.getReturnType());
+
+                if (typeEntity != null) {
+                    Operation op = new Operation();
+                    op.setClassStatic((method.getModifiers() & Modifier.STATIC) != 0);
+                    op.setName(method.getName());
+                    op.setVisibility(getVisibility(method));
+                    op.setType(typeEntity);
+
+                    Class<?>[] parms = method.getParameterTypes();
+
+                    for (int j = 0; j < parms.length; ++j) {
+                        Entity parmEntity = getEntity(model, parms[j]);
+
+                        if (parmEntity == null) {
+                            return;
+                        } else {
+                            op.getParameterList().add(parmEntity);
+                        }
+
+                    }
+
+                    entity.getOperations().add(op);
+
+                    // for (Class<?> exceptionClass :
+                    // method.getExceptionTypes())
+                    // {
+                    // Entity exceptionEntity = Entities.findEntity(model,
+                    // exceptionClass.getName());
+                    //
+                    // if (exceptionEntity == null)
+                    // {
+                    // exceptionEntity = new Entity();
+                    // exceptionEntity.setName(exceptionClass.getName());
+                    // model.getEntities().add(exceptionEntity);
+                    // }
+                    //
+                    // AssociationRelation relation = new
+                    // AssociationRelation(entity, exceptionEntity);
+                    // relation.setLabel("throws");
+                    // model.getRelations().add(relation);
+                    // }
+
+                }
+            }
+        } catch (NoClassDefFoundError ex) {
+            // ex.printStackTrace();
+
+        }
+    }
+
+    protected Visibility getVisibility(Member m) {
+        if ((m.getModifiers() & Modifier.PUBLIC) != 0) {
+            return Visibility.PUBLIC;
+        } else if ((m.getModifiers() & Modifier.PROTECTED) != 0) {
+            return Visibility.PROTECTED;
+        } else if ((m.getModifiers() & Modifier.PRIVATE) != 0) {
+            return Visibility.PRIVATE;
+        } else {
+            return Visibility.PRIVATE;
+        }
+    }
+
+    protected void fillModel(IModel model) {
+        for (Entity entity : new HashSet<Entity>(model.getEntities())) {
+            if (!entity.isPrimitive()) {
+                Class<?> clazz = entity_class.get(entity);
+                LOGGER.debug(clazz);
+                initInheritance(clazz, entity, model);
+                initAttributes(clazz, entity, model);
+                initOperations(clazz, entity, model);
+            }
+        }
+    }
+
+    public IModel createModel(File file) throws ParseError, IOException {
+        byte[] data = FileUtilities.getFileContent(file);
+
+        try {
+            return createModel(data);
+        } catch (ModelException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
