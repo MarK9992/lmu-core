d2d5591e91faa7c8a8f5dbb298a912a049d0be54
Marc Karassev <marc.karassev@yahoo.fr>
2016-01-26 09:29:43 +0100

    suppression de la partie plugin déplacée sur un autre repo

---
+++ out/inputAndLmuCore.java
@@ -0,0 +1,58 @@
+
+class ParseError
+{
+}
+
+
+class ModelExporter
+{
+}
+
+);
+}
+
+
+class Entity
+{
+}
+
+);
+}
+
+);
+}
+
+);
+}
+
+
+class Member
+{
+}
+
+
+class Exception
+{
+}
+
+
+class Visibility
+{
+}
+
+);
+}
+
+);
+}
+
+
+class IModel
+{
+}
+
+
+class NamedModelElement
+{
+}
+
